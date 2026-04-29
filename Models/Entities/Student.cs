ce6ff356ca99771d19d2f9b30fdb554799c028c7
Diana <diana@example.com>
2026-04-29 23:16:36 +0400

    Initial commit - Student Portal MVC application

---
+++ Models/Entities/Student.cs
@@ -0,0 +1,15 @@
+namespace Student_portal.Web.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool Subscription { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Student_portal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
